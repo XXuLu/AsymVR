8ca59489b10d6e4c49774eef15e85d23ca4755e0
HughLu <39286144+XXuLu@users.noreply.github.com>
2023-09-05 18:55:05 +0800

    修改vrgaze

--- Assets/Scripts/Gaze/VRGaze.cs
+++ Assets/Scripts/Gaze/VRGaze.cs
@@ -7,19 +7,20 @@ using ViveSR.anipal.Eye;
 public class VRGaze : MonoBehaviour
 {
     private Vector3 gazePoint;
+    
     private void Update()
     {
         
 
-        if (SRanipal_Eye_v2.Focus(GazeIndex.COMBINE, out Ray ray, out FocusInfo focusInfo))
+        if (SRanipal_Eye_v2.Focus(GazeIndex.COMBINE, out Ray ray, out FocusInfo focusInfo) && focusInfo.collider.gameObject.layer == 12)
         {
             // 判断射线焦点是否在当前物体上
-            if (focusInfo.collider == gameObject.GetComponent<Collider>() && focusInfo.collider.gameObject.layer == 12 )
-            {
+            
+            
                 gazePoint = focusInfo.point;
                 PhotonNetwork.Instantiate("gazeDot", gazePoint, Quaternion.identity);
 
-            }
+            
         }
 
         }
