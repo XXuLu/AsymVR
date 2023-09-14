74bb92786136dc926d6dce85386e2a12704cdfb6
HughLu <39286144+XXuLu@users.noreply.github.com>
2023-09-14 18:33:00 +0800

    vrgazework

--- Assets/Scripts/Gaze/GazeTrail.cs
+++ Assets/Scripts/Gaze/GazeTrail.cs
@@ -1,35 +1,86 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using ViveSR.anipal.Eye;
 
 public class GazeTrail : MonoBehaviourPunCallbacks
 {
-    public float distance = 5f;
+    public float distance = 2f;
     public Vector3 offset = Vector3.zero;
 
+    private static EyeData_v2 eyeData = new EyeData_v2();
+    private bool eye_callback_registered = false;
     private void Start()
     {
         Cursor.visible = false;
+        if (!SRanipal_Eye_Framework.Instance.EnableEye)
+        {
+            enabled = false;
+            return;
+        }
+        
     }
+
+   
     // Update is called once per frame
     void Update()
     {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING && SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
 
-        if (photonView.IsMine)
+        if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
         {
-            if (SRanipal_Eye_v2.Focus(GazeIndex.COMBINE, out Ray VRray, out FocusInfo focusInfo))
-            {
-                Vector3 newVRPosition = VRray.GetPoint(distance);
-                transform.position = newVRPosition + offset;
-            }
-            else
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Vector3 newPosition = ray.GetPoint(distance);
-                transform.position = newPosition + offset;
-            }
+            SRanipal_Eye_v2.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = true;
         }
+        else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 newPosition = ray.GetPoint(distance);
+            transform.position = newPosition + offset;
+            
+               
+      
+        }
+
+        Vector3 GazeOriginCombinedLocal, GazeDirectionCombinedLocal;
+
+        if (eye_callback_registered)
+        {
+            if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else return;
+        }
+        else
+        {
+            if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else if (SRanipal_Eye_v2.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else return;
+        }
+
+        Vector3 GazeDirectionCombined = Camera.main.transform.TransformDirection(GazeDirectionCombinedLocal);
+        transform.position = Camera.main.transform.position + GazeDirectionCombined * distance + offset;
+
+
+
+    }
+
+    private void Release()
+    {
+        if (eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+    private static void EyeCallback(ref EyeData_v2 eye_data)
+    {
+        eyeData = eye_data;
     }
 }
